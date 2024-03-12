ebc5429b9cc9949a548b3b0155ddc2c5286d7182
Dhairya <shahdhairya101@gmail.com>
2024-03-11 19:25:48 -0700

    week 2 day 1 homework

---
+++ m3-w2-d1-homework/MongoDB/app.js
@@ -0,0 +1,44 @@
+var stats =[
+    {
+        'city': 'San Juan', 
+        'zip': '00926', 
+        'state': 'PR', 
+        'income': '34781',
+        'age': '44'
+    },
+    {
+        'city': 'Corona', 
+        'zip': '11368', 
+        'state': 'NY', 
+        'income': '50797',
+        'age': '32'
+    },
+    {
+        'city': 'Chicago', 
+        'zip': '60629', 
+        'state': 'IL', 
+        'income': '42019',
+        'age': '31'
+    },
+    {
+        'city': 'El Paso', 
+        'zip': '79936', 
+        'state': 'TX', 
+        'income': '54692',
+        'age': '31'
+    },
+    {
+        'city': 'Los Angeles', 
+        'zip': '90011', 
+        'state': 'CA', 
+        'income': '36954',
+        'age': '28'
+    },
+    {
+        'city': 'Norwalk', 
+        'zip': '90650', 
+        'state': 'CA', 
+        'income': '66453',
+        'age': '35'
+    }
+]
