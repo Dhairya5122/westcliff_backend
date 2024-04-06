5e6a71aff5d8404e1f258752bf5418a6c4594860
Dhairya <shahdhairya101@gmail.com>
2024-04-06 13:13:15 -0700

    week 5 day 3 homework

--- wk4-day3-assignment/registration-form/app.js
+++ wk4-day3-assignment/registration-form/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const routes = require('./routes/index');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', routes);
+app.use("/", routes);
 
 module.exports = app;
